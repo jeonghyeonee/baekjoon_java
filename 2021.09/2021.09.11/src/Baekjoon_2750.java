fc26f097fc24cdb9b6e62fe61a5bef796016da96
jeonghyeonee <33801356+jeonghyeonee@users.noreply.github.com>
2021-09-11 23:26:36 +0900

    2021.09.11.
    수 정렬하기

--- 2021.09/2021.09.11/src/Baekjoon_2750.java
+++ 2021.09/2021.09.11/src/Baekjoon_2750.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Baekjoon_2750 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+        
         int m = 0;
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++)
