49b37a2a54bd6c8029a32f25afcb9bc3fbc32843
jeonghyeonee <jlh1218@naver.com>
2022-02-22 21:25:45 +0900

    2022.02.22

---
+++ 2022/2022.02/2022.02.22/src/Baekjoon_11650.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_11650 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            // 문제가 이해가 안되는데 큰일이네...^^
+        }
+    }
+}
