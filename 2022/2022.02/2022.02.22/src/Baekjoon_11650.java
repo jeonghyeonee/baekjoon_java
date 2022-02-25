bf6e520ba828446b3ecb73e13e31edd76ee3845f
jeonghyeonee <jlh1218@naver.com>
2022-02-25 23:53:52 +0900

    2022.02.25.

--- 2022/2022.02/2022.02.22/src/Baekjoon_11650.java
+++ 2022/2022.02/2022.02.22/src/Baekjoon_11650.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Baekjoon_11650 {
@@ -15,6 +16,21 @@ public class Baekjoon_11650 {
             }
         }
 //        자바에서 lambda 쓸 수 있나?
-        Arrays.sort(arr);
+//        https://st-lab.tistory.com/110 
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }
+                else{
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
     }
 }
