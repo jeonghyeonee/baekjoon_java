6c88801d6e3f4ebe5ca70106b27c36510301d11a
jeonghyeonee <jlh1218@naver.com>
2022-02-28 01:00:58 +0900

    2022.02.27.

---
+++ 2022/2022.02/2022.02.27/src/Baekjoon_1181.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Baekjoon_1181 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String arr[] = new String[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.next();
+        }
+        // 같은 단어에 대해서 어떻게 처리할 것인가? -> 집합?
+
+    }
+}
