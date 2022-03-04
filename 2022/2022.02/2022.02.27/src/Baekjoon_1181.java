d8b3d91eeea2ac64b2a3d556af5a5e0f6c21efdb
jeonghyeonee <jlh1218@naver.com>
2022-03-04 23:12:29 +0900

    2022.03.04.

--- 2022/2022.02/2022.02.27/src/Baekjoon_1181.java
+++ 2022/2022.02/2022.02.27/src/Baekjoon_1181.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Baekjoon_1181 {
@@ -5,12 +7,35 @@ public class Baekjoon_1181 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+
+        sc.nextLine();
+
         String arr[] = new String[n];
 
         for(int i=0; i<n; i++){
             arr[i] = sc.next();
         }
         // 같은 단어에 대해서 어떻게 처리할 것인가? -> 집합?
+        // Comparator 좀 더 알아보기!!!!
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()){
+                    return o1.compareTo(o2);    //사전 순 정렬
+                }
+                else{
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(arr[0]);
+
+        for(int i=1; i<n; i++){
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
 
     }
 }
