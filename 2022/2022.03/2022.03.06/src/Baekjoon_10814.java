7325409d9fb461266d08974c9812293341c73b9f
jeonghyeonee <jlh1218@naver.com>
2022-03-07 10:15:33 +0900

    2022.03.07.

--- 2022/2022.03/2022.03.06/src/Baekjoon_10814.java
+++ 2022/2022.03/2022.03.06/src/Baekjoon_10814.java
@@ -1,3 +1,6 @@
+// Hashmap을 사용할 경우 같은 나이의 사람들을 못 저장 -> 2차원 배열 사용
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -6,15 +9,23 @@ public class Baekjoon_10814 {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
+        String[][] arr = new String[N][2];
 
-        sc.nextLine();
-
-        HashMap<Integer, String> info = new HashMap<>();
-
-        for(int i=0; i<N; i++){
-            info.put(sc.nextInt(), sc.next());
+        for (int i=0; i<N; i++){
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
         }
 
-        
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0]));
+            }
+        });
+
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
     }
 }
