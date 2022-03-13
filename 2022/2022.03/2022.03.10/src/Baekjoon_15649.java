d5f0effcb1d80ea059bc69b9fc37065540d716ad
jeonghyeonee <jlh1218@naver.com>
2022-03-13 22:40:14 +0900

    2022.03.13.

--- 2022/2022.03/2022.03.10/src/Baekjoon_15649.java
+++ 2022/2022.03/2022.03.10/src/Baekjoon_15649.java
@@ -1,12 +1,42 @@
+//https://st-lab.tistory.com/114
+//bactraking 과 dfs 개념!
+
 import java.util.Scanner;
 
 public class Baekjoon_15649 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
         int M = sc.nextInt();
 
+        arr = new int[M];
+        visit = new boolean[N];
+        backtracking(N, M, 0);
+
+    }
+
+    public static void backtracking(int N, int M, int depth){
+        if (depth == M){
+            for (int i : arr){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            if (!visit[i]){
+                visit[i] = true;
+                arr[depth] = i+1;
+                backtracking(N, M, depth+1);
+                visit[i] = false;
+            }
+        }
 
     }
 }
