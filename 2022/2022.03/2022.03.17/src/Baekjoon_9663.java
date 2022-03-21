15e3c17c2038cb599a911ea15bc66f0d56d0ae43
jeonghyeonee <jlh1218@naver.com>
2022-03-21 23:53:43 +0900

    2022.03.21.

--- 2022/2022.03/2022.03.17/src/Baekjoon_9663.java
+++ 2022/2022.03/2022.03.17/src/Baekjoon_9663.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Baekjoon_9663 {
     public static int N;
+    public static int[] arr;
+    public static int cnt=0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +13,8 @@ public class Baekjoon_9663 {
 
     }
 
-    public static void ck(){
+    public static void ck(int depth){
+
 
     }
 
