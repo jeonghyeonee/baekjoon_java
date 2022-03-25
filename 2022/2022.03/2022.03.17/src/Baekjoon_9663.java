fe1a9e279e9fa5cb509f389ef61e0325bb003732
jeonghyeonee <jlh1218@naver.com>
2022-03-25 16:59:01 +0900

    2022.03.25.

--- 2022/2022.03/2022.03.17/src/Baekjoon_9663.java
+++ 2022/2022.03/2022.03.17/src/Baekjoon_9663.java
@@ -1,24 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Baekjoon_9663 {
     public static int N;
     public static int[] arr;
     public static int cnt=0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
 
-
-    }
-
-    public static void ck(int depth){
-
+        nQueen(0);
+        System.out.println(cnt);
 
     }
 
-    public static void backtracking(){
+    public static void nQueen(int depth){
+        if (depth == N){
+            cnt++;
+            return;
+        }
 
+        for(int i=0; i<N; i++){
+            arr[depth] = i;
+            if(Possibility(depth)){
+                nQueen(depth+1);
+            }
+        }
+    }
+
+    public static boolean Possibility(int col){
+        for(int i=0; i<col; i++){
+            if (arr[col] == arr[i]){
+                return false;
+            }
+
+            else if(Math.abs(col-i) == Math.abs(arr[col]-arr[i])){
+                return false;
+            }
+        }
+        return true;
     }
 }
