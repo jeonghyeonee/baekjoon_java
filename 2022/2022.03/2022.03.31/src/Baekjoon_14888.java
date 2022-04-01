b0c00c7a0d6232122b0336e1f75341b1e0ce9a7d
jeonghyeonee <jlh1218@naver.com>
2022-04-01 12:48:40 +0900

    2022.04.01.

--- 2022/2022.03/2022.03.31/src/Baekjoon_14888.java
+++ 2022/2022.03/2022.03.31/src/Baekjoon_14888.java
@@ -8,6 +8,8 @@ public class Baekjoon_14888 {
     public static int N;
     public static int[] num;
     public static int[] operator = new int[4];
+    public static int MAX = Integer.MIN_VALUE;
+    public static int MIN = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,13 +25,47 @@ public class Baekjoon_14888 {
         }
 
         // 연산자 입력
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i=0; i<4; i++){
             operator[i] = Integer.parseInt(st.nextToken());
         }
 
+        calculator(num[0], 1);
+
+        System.out.println(MAX);
+        System.out.println(MIN);
+
     }
-    
-    public static void calculator{
+
+    public static void calculator(int n, int idx){
+        if (idx == N){
+            MAX = Math.max(MAX, n);
+            MIN = Math.min(MIN, n);
+            return;
+        }
+
+        for(int i=0; i<4; i++){
+            if(operator[i] > 0){
+                operator[i]--;
+
+                switch (i){
+                    case 0:
+                        calculator(n+num[idx], idx+1);
+                        break;
+                    case 1:
+                        calculator(n-num[idx], idx+1);
+                        break;
+                    case 2:
+                        calculator(n*num[idx], idx+1);
+                        break;
+                    case 3:
+                        calculator(n/num[idx], idx+1);
+                        break;
+                }
+
+                operator[i]++;
+            }
+        }
 
     }
 }
