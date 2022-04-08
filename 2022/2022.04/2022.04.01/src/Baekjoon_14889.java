6819df8557c8545d692f260021646f28c2c0a541
jeonghyeonee <jlh1218@naver.com>
2022-04-08 10:45:47 +0900

    2022.04.08.

--- 2022/2022.04/2022.04.01/src/Baekjoon_14889.java
+++ 2022/2022.04/2022.04.01/src/Baekjoon_14889.java
@@ -1,9 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Baekjoon_14889 {
-    public static void main(String[] args) {
+
+    public static int N;
+    public static int[][] score;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        score = new int[N][N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                score[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+    }
+
+    public static void plus(int n, int ability){
+
     }
 }
