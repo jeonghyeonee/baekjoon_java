d3bfdf9f49ab520564a594f72b8920ae0c422e7c
jeonghyeonee <jlh1218@naver.com>
2022-04-24 21:48:48 +0900

    2022.04.24.

--- 2022/2022.04/2022.04.19/src/Baekjoon_1003.java
+++ 2022/2022.04/2022.04.19/src/Baekjoon_1003.java
@@ -4,43 +4,38 @@ import java.io.InputStreamReader;
 
 public class Baekjoon_1003 {
 
-    public static int[] zero = new int[41];
-    public static int[] one = new int[41];
+    public static Integer[][] dp = new Integer[41][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        zero[0] = 1;
-        zero[1] = 0;
-        zero[2] = 1;
-
-        one[0] = 0;
-        one[1] = 1;
-        one[2] = 1;
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
 
         int T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
 
-        while(T-->0){
+        while(T --> 0){
+            int N = Integer.parseInt(br.readLine());
             int N = Integer.parseInt(br.readLine());
             fibo(N);
-            sb.append(zero[N]+ " " + one[N]).append('\n');
+            sb.append(dp[N][0]+ " " + dp[N][1]).append('\n');
         }
         System.out.println(sb);
 
     }
 
-    public static void fibo(int N){
-        if (3 <= N){
-            for (int i=3; i<=N; i++){
-                zero[N] = zero[N-1] + zero[N-2];
-                one[N] = one[N-1] + one[N-2];
-            }
+    public static Integer[] fibo(int N){
+        if (dp[N][0] == null || dp[N][1] == null){
+            dp[N][0] = fibo(N-1)[0] + fibo(N-2)[0];
+            dp[N][1] = fibo(N-1)[1] + fibo(N-2)[1];
         }
-        return;
+        return dp[N];
     }
 }
 
 // dp array로 다시 풀어보기...
-// 2 6 22 > 0 0 / 0 0 
+// 2 6 22 > 0 0 / 0 0
