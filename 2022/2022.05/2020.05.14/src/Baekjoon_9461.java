dc5b8aa83acca2a86948fe084d55f4ba7bfa8ea1
jeonghyeonee <jlh1218@naver.com>
2022-05-14 18:54:06 +0900

    2022.05.14.

---
+++ 2022/2022.05/2020.05.14/src/Baekjoon_9461.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_9461 {
+
+    public static int[] dp = new int[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+
+    }
+}
