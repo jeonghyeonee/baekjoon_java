2fffc3647805acf1f3ad0c71fa699995d619d6ac
jeonghyeonee <jlh1218@naver.com>
2022-06-16 22:22:26 +0900

    2022.06.16.

---
+++ 2022/2022.06/2022.06.16/src/Baekjoon_1932.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1932 {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//        반복문으로 풀어볼까??
+    }
+}
