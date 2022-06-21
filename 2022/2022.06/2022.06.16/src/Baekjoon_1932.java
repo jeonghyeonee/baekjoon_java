d61cf6b02e8f99553dfb261e1fa5d89749cef2ba
jeonghyeonee <jlh1218@naver.com>
2022-06-21 23:57:11 +0900

    2022.06.21.

--- 2022/2022.06/2022.06.16/src/Baekjoon_1932.java
+++ 2022/2022.06/2022.06.16/src/Baekjoon_1932.java
@@ -1,15 +1,26 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon_1932 {
 
-    public static void main(String[] args) {
+    public static Integer[][] dp;
+    public static int[][] arr;
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] dp;
+        N = Integer.parseInt(br.readLine());
 
+        arr = new int[N][N];
+        dp = new Integer[N][N];
         StringTokenizer st;
+        
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+        }
 
     }
 }
