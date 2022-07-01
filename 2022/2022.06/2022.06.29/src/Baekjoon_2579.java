e2849165d32b472f0d2dfcd44b81101577deb371
jeonghyeonee <jlh1218@naver.com>
2022-07-01 17:52:38 +0900

    2022.07.01.

--- 2022/2022.06/2022.06.29/src/Baekjoon_2579.java
+++ 2022/2022.06/2022.06.29/src/Baekjoon_2579.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,5 +13,22 @@ public class Baekjoon_2579 {
 
         int[] dp = new int[N+1];
         int[] arr = new int[N+1];
+
+        for(int i=1; i<=N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = arr[1];
+
+        if(N>=2){
+            dp[2] = arr[1] + arr[2];
+        }
+
+        for(int i=3; i<=N; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3]+arr[i-1])+arr[i];
+        }
+        System.out.println(dp[N]);
     }
+
+
 }
