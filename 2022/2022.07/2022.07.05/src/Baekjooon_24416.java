fd640dcac763680b4d71da5f7ed774fbeb22c58c
jeonghyeonee <jlh1218@naver.com>
2022-07-09 23:22:57 +0900

    2022.07.09.

--- 2022/2022.07/2022.07.05/src/Baekjooon_24416.java
+++ 2022/2022.07/2022.07.05/src/Baekjooon_24416.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 public class Baekjooon_24416 {
 
     static int[] dp;
+    static int cnt1 = 0;
+    static int cnt2 = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +18,7 @@ public class Baekjooon_24416 {
         dp[1] = 1;
         dp[2] = 1;
 
-        System.out.println(fibo(N));
+        System.out.println(fib(N) + " " + fibo(N));
 
 
     }
@@ -24,9 +26,19 @@ public class Baekjooon_24416 {
     static int fibo(int N){
         for(int i=3; i<(N+1); i++){
             dp[i] = dp[i-1] + dp[i-2];
+            cnt2++;
         }
+        return cnt2;
+    }
 
-        return dp[N];
+    static int fib(int N){
+        cnt1++;
+        if (N==1 || N==2){
+            return 1;
+        }
+        else{
+            return (fib(N-1) + fib(N-2));
+        }
     }
 
 
