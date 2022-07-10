07cfb9c9e4e2e4072fe37acffa30e194a0e30b67
jeonghyeonee <jlh1218@naver.com>
2022-07-10 23:32:30 +0900

    2022.07.10.

---
+++ 2022/2022.07/2022.07.10/src/Baekjoon_10844.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_10844 {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new int[N];
+
+//        호오,,,문제가 생각보다 이해가 안 가는 걸?
+    }
+}
