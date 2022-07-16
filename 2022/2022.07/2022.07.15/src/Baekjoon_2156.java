df7ec98fa18794a80cefbdd06538eb69df819e21
jeonghyeonee <jlh1218@naver.com>
2022-07-16 17:43:33 +0900

    2022.07.16.

--- 2022/2022.07/2022.07.15/src/Baekjoon_2156.java
+++ 2022/2022.07/2022.07.15/src/Baekjoon_2156.java
@@ -5,13 +5,24 @@ import java.util.StringTokenizer;
 
 public class Baekjoon_2156 {
 
+    static Integer[] dp;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        dp = new Integer[N+1];
+        arr = new int[N+1];
 
-        
+        for(int i=1; i<N+1; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[0] = 0;
+        dp[1] = arr[1];
+
+
     }
 }
