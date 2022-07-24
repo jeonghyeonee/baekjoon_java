dfa4b005152f31fd4f748a884b0d5d76874cfb93
jeonghyeonee <jlh1218@naver.com>
2022-07-24 21:21:25 +0900

    2022.07.24.

--- 2022/2022.07/2022.07.19/src/Baekjoon_11053.java
+++ 2022/2022.07/2022.07.19/src/Baekjoon_11053.java
@@ -26,9 +26,22 @@ public class Baekjoon_11053 {
             LTS(i);
         }
 
+        int max = dp[0];
+
     }
 
     static int LTS(int N){
-        
+
+        if(dp[N] == null){
+            dp[N] = 1;
+
+            for(int i=N-1; i>=0; i--){
+                if(seq[i] < seq[N]){
+                    dp[N] = Math.max(dp[N], LTS(i) + 1);
+                }
+            }
+        }
+        return dp[N];
+
     }
 }
