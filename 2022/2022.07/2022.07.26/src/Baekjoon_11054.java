5d64dfab8ff95fdd154bb7d693fa95d11932a1e4
jeonghyeonee <jlh1218@naver.com>
2022-08-05 13:43:33 +0900

    2022.08.05.

--- 2022/2022.07/2022.07.26/src/Baekjoon_11054.java
+++ 2022/2022.07/2022.07.26/src/Baekjoon_11054.java
@@ -23,6 +23,17 @@ public class Baekjoon_11054 {
         for(int i=0; i<N; i++){
             seq[i] = Integer.parseInt(st.nextToken());
         }
+
+        for(int i=0; i<N; i++){
+            LTS(i);
+            LDS(i);
+        }
+
+        int max = -1;
+        for(int i=0; i<N; i++){
+            max = Math.max(r_dp[i] + l_dp[i], max);
+        }
+        System.out.println(max-1);
     }
 
     static int LTS(int N){
@@ -41,8 +52,15 @@ public class Baekjoon_11054 {
     static int LDS(int N){
         if(l_dp[N] == null){
             l_dp[N] = 1;
+            for(int i=N+1; i<l_dp.length; i++){
+                if(seq[i] < seq[N]){
+                    l_dp[N] = Math.max(l_dp[N], LDS(i)+1);
+                }
+            }
         }
+        return l_dp[N];
     }
+
 
 
 }
