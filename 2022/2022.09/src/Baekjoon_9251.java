7e8f193d55d7b34bbfb3a62f8335ed4e8a388247
jeonghyeonee <jlh1218@naver.com>
2022-09-04 22:40:39 +0900

    2022.09.04.

---
+++ 2022/2022.09/src/Baekjoon_9251.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_9251 {
+
+    static int[] dp;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
