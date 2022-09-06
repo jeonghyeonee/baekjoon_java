73177fddf0170d297e8d73c3f2ea518fa7ba82ea
jeonghyeonee <jlh1218@naver.com>
2022-09-06 23:30:21 +0900

    2022.09.06.

--- 2022/2022.09/src/Baekjoon_9251.java
+++ 2022/2022.09/src/Baekjoon_9251.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon_9251 {
@@ -6,9 +7,10 @@ public class Baekjoon_9251 {
     static char[] str1;
     static char[] str2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
+        str1 = br.readLine().toCharArray();
+        str2 = br.readLine().toCharArray();
     }
 }
