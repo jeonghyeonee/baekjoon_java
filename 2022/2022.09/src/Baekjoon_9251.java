e91c6b90bd0547a615bb747a3f377283c2fcd5c0
jeonghyeonee <jlh1218@naver.com>
2022-09-16 16:01:25 +0900

    2022.09.16.

--- 2022/2022.09/src/Baekjoon_9251.java
+++ 2022/2022.09/src/Baekjoon_9251.java
@@ -7,10 +7,20 @@ public class Baekjoon_9251 {
     static char[] str1;
     static char[] str2;
 
+    static Integer[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         str1 = br.readLine().toCharArray();
         str2 = br.readLine().toCharArray();
+
+        dp = new Integer[str1.length][str2.length];
+
+        System.out.println();
+    }
+
+    static int LCS(int x, int y){
+        
     }
 }
